c17c2cbd365ec0863050b0105e8c4807e85a4863
Goopi07 <mandhate.gopinath.com>
2025-03-04 22:56:37 +0530

    in out

--- routes/reportImportDataToDB.js
+++ routes/reportImportDataToDB.js
@@ -44,7 +44,7 @@ router.post("/:rollNumber", async (req, res) => {
                         date: inputDate,
                     });
                     await report.save();
-                    return res.json({ message: "Late entry stored successfully", lateEntry, name: report.name, date: report.date, rollNumber: report.rollNumber, branch: report.branch });
+                    return res.json({ message: "Late entry stored successfully", lateEntry, name: report.name, date: report.date, rollNumber: report.rollNumber, branch: report.branch,Intime:report.checkInTime });
                 } else {
                     return res.status(400).json({ message: "Already checked in for today." });
                 }
@@ -70,13 +70,13 @@ router.post("/:rollNumber", async (req, res) => {
                         date: inputDate,
                     });
                     await report.save();
-                    return res.json({ message: "Early exit stored successfully", earlyExit  ,name: report.name, date: report.date, rollNumber: report.rollNumber, branch: report.branch});
+                    return res.json({ message: "Early exit stored successfully", earlyExit  ,name: report.name, date: report.date, rollNumber: report.rollNumber, branch: report.branch,OutTime:report.checkOutTime,});
                 } 
                 else if (!report.checkOutTime) {
                     report.checkOutTime = currentTime;
                     report.earlyExitDuration = earlyExit;
                     await report.save();
-                    return res.json({ message: "Early exit updated successfully", earlyExit, name: report.name, date: report.date, rollNumber: report.rollNumber, branch: report.branch });
+                    return res.json({ message: "Early exit updated successfully", earlyExit, name: report.name, date: report.date, rollNumber: report.rollNumber, branch: report.branch,OutTime:report.checkOutTime, });
                 }
                 else {
                     return res.status(400).json({ message: "Already checked out today." });
